93d7c46d895875bcd48b920e12bd49b750ebc71f
NigRG <jcazman19@yahoo.com>
2022-03-01 17:09:58 -0500

    Extra Check

--- Memory/Hooks.cpp
+++ Memory/Hooks.cpp
@@ -286,7 +286,7 @@ bool Hooks::playerCallBack(C_Player* lp, __int64 a2, __int64 a3) {
 				if (info.State & MEM_FREE) continue;
 				if (info.State & MEM_RESERVE) continue;
 
-				if (*(__int64*)ent.ent != 0xFFFFFFFFFFFFFCD7 && entity != nullptr && ent.ent != nullptr && entity->isAlive() && *(__int64*)ent.ent > 0x6FF000000000 && *(__int64*)ent.ent < 0x800000000000 && *((int64_t*)ent.ent + 1) < 0x6FF000000000 && *(__int64*)ent.ent <= Utils::getBase() + 0x10000000)
+				if (entity != nullptr && (__int64)entity != 0xFFFFFFFFFFFFFCD7 && ent.ent != nullptr && entity->isAlive() && *(__int64*)ent.ent != 0xFFFFFFFFFFFFFCD7 && *(__int64*)ent.ent > 0x6FF000000000 && *(__int64*)ent.ent < 0x800000000000 && *((int64_t*)ent.ent + 1) < 0x6FF000000000 && *(__int64*)ent.ent <= Utils::getBase() + 0x10000000)
 					validEntities.push_back(ent);
 			}
 			g_Hooks.entityList.clear();
