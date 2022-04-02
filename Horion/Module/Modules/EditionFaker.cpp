4a325db335eb3edc19b59cb0fe5b4459ec46803f
Smint <ssg.jss2018@gmail.com>
2022-04-02 09:52:28 +0000

    [Update] Add More Options to EditionFaker

--- Horion/Module/Modules/EditionFaker.cpp
+++ Horion/Module/Modules/EditionFaker.cpp
@@ -5,12 +5,18 @@ EditionFaker::EditionFaker() : IModule(0, Category::MISC, "Fakes your device") {
 				.addEntry(EnumEntry("Unknown", 0))
 				.addEntry(EnumEntry("Android", 1))
 				.addEntry(EnumEntry("iOS", 2))
-				.addEntry(EnumEntry("MacOS", 3))
-				.addEntry(EnumEntry("Win10", 7))
-				.addEntry(EnumEntry("Windows", 8))
-				.addEntry(EnumEntry("Linux", 9))
+				.addEntry(EnumEntry("macOS", 3))
+				.addEntry(EnumEntry("FireOS", 4))
+				.addEntry(EnumEntry("GearVR", 5))
+				.addEntry(EnumEntry("Hololens", 6))
+				.addEntry(EnumEntry("Win10", 7)) // Windows 10 (PC)
+				.addEntry(EnumEntry("Win32", 8)) // Windows
+				.addEntry(EnumEntry("Linux", 9)) // Dedicated
+				.addEntry(EnumEntry("TvOS", 10))
+				.addEntry(EnumEntry("Playstation", 11)) // Orbis
+				.addEntry(EnumEntry("Nintendo Switch", 12)) // NX								
 				.addEntry(EnumEntry("Xbox", 13))
-				.addEntry(EnumEntry("WindowsPhone", 14));
+				.addEntry(EnumEntry("Windows Phone", 14));
 		registerEnumSetting("Edition", &edition, 1);
 }
 
