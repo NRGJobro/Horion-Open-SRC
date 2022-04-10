3442bfc31fa5a9e82886215ed514c724ee99374e
Xello12121 <Xello12121@gmail.com>
2022-04-09 23:13:20 -0500

    part 2

--- Horion/Module/Modules/ExtendedBlockReach.cpp
+++ Horion/Module/Modules/ExtendedBlockReach.cpp
@@ -1,12 +1,12 @@
 #include "ExtendedBlockReach.h"
 
 ExtendedBlockReach::ExtendedBlockReach() : IModule(0, Category::PLAYER, "Exteneded reach for placing/breaking blocks") {
-	registerFloatSetting(std::string("reach"), &blockReach, blockReach, 7, 20);
+	registerFloatSetting(std::string("Reach"), &blockReach, blockReach, 7, 20);
 }
 
 ExtendedBlockReach::~ExtendedBlockReach() {
 }
 
 const char* ExtendedBlockReach::getModuleName() {
-	return ("ExtendedBlockReach");
+	return ("BlockReach");
 }
