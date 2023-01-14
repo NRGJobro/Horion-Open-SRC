56c34523d9d6c8f893d9b537e27aab00f43e1cd3
Intoprelised <78622918+Intoprelised@users.noreply.github.com>
2023-01-14 15:05:55 -0500

    Clean up a little bit - First commit in like months

--- Horion/Module/Modules/Teleport.cpp
+++ Horion/Module/Modules/Teleport.cpp
@@ -5,23 +5,19 @@ Teleport::Teleport() : IModule(0, Category::MISC, "Click a block to teleport to 
 	registerBoolSetting("Push", &bypass, bypass);
 }
 
-Teleport::~Teleport() {
-}
+Teleport::~Teleport() {}
 
 const char* Teleport::getModuleName() {
 	return "Teleport";
 }
 
 void Teleport::onTick(GameMode* gm) {
-	if (!GameData::canUseMoveKeys())
-		return;
-	if (onlyHand && Game.getLocalPlayer()->getSupplies()->inventory->getItemStack(Game.getLocalPlayer()->getSupplies()->selectedHotbarSlot)->item != nullptr)
-		return;
+	if (!GameData::canUseMoveKeys()) return;
+	if (onlyHand && Game.getLocalPlayer()->getSupplies()->inventory->getItemStack(Game.getLocalPlayer()->getSupplies()->selectedHotbarSlot)->item != nullptr) return;
 
 	if (GameData::isRightClickDown() && !hasClicked) {
 		hasClicked = true;
-
-		Vec3i block = Game.getLocalPlayer()->getlevel()->block;
+		const Vec3i& block = Game.getLocalPlayer()->getlevel()->block;
 		if (block == Vec3i(0, 0, 0)) return;
 		Vec3 pos = block.toFloatVector();
 		pos.x += 0.5f;
@@ -32,25 +28,15 @@ void Teleport::onTick(GameMode* gm) {
 
 		Game.getGuiData()->displayClientMessageF("%sTeleport position set to %sX: %.1f Y: %.1f Z: %.1f%s. Sneak to teleport!", GREEN, GRAY, pos.x, pos.y, pos.z, GREEN);
 	}
-	if (!GameData::isRightClickDown())
-		hasClicked = false;
+	if (!GameData::isRightClickDown()) hasClicked = false;
 
-	GameSettingsInput* input = Game.getClientInstance()->getGameSettingsInput();
-	if (shouldTP && GameData::isKeyDown(*input->sneakKey)) {
+	if (shouldTP && GameData::isKeyDown(*Game.getClientInstance()->getGameSettingsInput()->sneakKey)) {
 		tpPos.y += (gm->player->getPos()->y - gm->player->getAABB()->lower.y) + 1;  // eye height + 1
 		if (bypass) {
-			/*int dist = (int)gm->player->getPos()->dist(tpPos);
-			int i = (int)dist / 5;
-			for (int n = 0; n < i; n++) {
-				Vec3 offs = tpPos.sub(*gm->player->getPos()).div(i).mul(n);
-				C_MovePlayerPacket p = C_MovePlayerPacket(Game.getLocalPlayer(), gm->player->getPos()->add(offs));
-				Game.getClientInstance()->loopbackPacketSender->sendToServer(&p);
-			}
-			gm->player->setPos(tpPos);*/
-			float dist = gm->player->getPos()->dist(tpPos);
-			Game.getLocalPlayer()->lerpTo(tpPos, Vec2(1, 1), (int)fmax((int)dist * 0.1, 1));
-		} else
+			Game.getLocalPlayer()->lerpTo(tpPos, Vec2(1, 1), (int)std::fmax((int)(gm->player->getPos()->dist(tpPos) * 0.1), 1));
+		} else {
 			gm->player->setPos(tpPos);
+		}
 		shouldTP = false;
 	}
 }
