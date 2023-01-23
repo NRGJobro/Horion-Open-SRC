7c06be4809545f4a2bf20370087ace3d56e4b258
NRGJobro <85864573+NRGJobro@users.noreply.github.com>
2023-01-23 17:15:22 -0500

    Yes

--- SDK/Entity.h
+++ SDK/Entity.h
@@ -46,18 +46,14 @@ public:
 
 	void playSound(std::string sound, Vec3 const &position, float volume, float pitch) {
 		using playSound_t = void(__fastcall *)(Level *, TextHolder *, Vec3 const &, float, float);
-		// Using getBase so there's a much smaller and barely noticeable delay when we call this function for the first time
 		static playSound_t func = reinterpret_cast<playSound_t>(Utils::getBase() + 0x2447650);  // 48 89 5c 24 ? 48 89 6c 24 ? 48 89 74 24 ? 57 48 83 ec ? 48 8b 81 ? ? ? ? 33 ff 48 2b 81 ? ? ? ? 49 8b f0 48 c1 f8 ? 48 8b ea
-		if (func != 0) {
-			TextHolder th(sound);
-			func(this, &th, position, volume, pitch);
-		}
+		if (func != nullptr) func(this, &TextHolder(sound), position, volume, pitch);
 	}
 
 	// Credits to hacker hansen for this
 	std::vector<Entity *> getMiscEntityList() {
 		using entityList_t = std::int64_t *(__fastcall *)(Level *, void *);
-		static entityList_t func = reinterpret_cast<entityList_t>(Utils::getBase() + 0x2451510);
+		static entityList_t func = reinterpret_cast<entityList_t>(Utils::getBase() + 0x2451510); // 48 89 5c 24 ? 56 57 41 56 48 83 ec ? 48 8b 05 ? ? ? ? 48 33 c4 48 89 44 24 ? 48 8b f2 4c 8b f1 48 89 54 24 ? 33 c9
 		if (func != nullptr) {
 			std::unique_ptr<char[]> alloc = std::make_unique<char[]>(0x18);
 			std::int64_t *listStart = func(this, alloc.get());
@@ -66,9 +62,7 @@ public:
 			std::vector<Entity *> res;
 			res.reserve(listSize);
 			if (listSize > 0 && listSize < 5000) {
-				for (std::size_t i = 0; i < listSize; i++) {
-					res.push_back(entityList[i]);
-				}
+				for (std::size_t i = 0; i < listSize; i++) res.push_back(entityList[i]);
 			}
 			return res;
 		}
