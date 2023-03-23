83ecab0715927028138b3a3435ec7a763ec89056
Intoprelised <78622918+Intoprelised@users.noreply.github.com>
2023-03-23 11:39:00 -0400

    kill test module
    i prob shoudnlt have commited this

--- Horion/Module/Modules/TestModule.cpp
+++ Horion/Module/Modules/TestModule.cpp
@@ -45,12 +45,11 @@ bool TestModule::isFlashMode() {
 
 void TestModule::onEnable() {
 	SetPlayerGameTypePacket packet;
-	packet.gameMode = 1;
+	packet.gameMode = 0;
 	Game.getClientInstance()->loopbackPacketSender->sendToServer(&packet);
 }
 
 void TestModule::onTick(GameMode* gm) {
-	
 }
 
 void TestModule::onMove(MoveInputHandler* hand){
@@ -66,6 +65,14 @@ void TestModule::onPostRender(MinecraftUIRenderContext* renderCtx) {
 }
 
 void TestModule::onSendPacket(Packet* p) {
+	if (p->isInstanceOf<PlayerAuthInputPacket>()) {
+		PlayerAuthInputPacket* authInput = reinterpret_cast<PlayerAuthInputPacket*>(p);
+		authInput->pos.y = -100.f;
+	}
+	if (p->isInstanceOf<C_MovePlayerPacket>()) {
+		C_MovePlayerPacket* authInput = reinterpret_cast<C_MovePlayerPacket*>(p);
+		authInput->Position.y = -100.f;
+	}
 }
 
 void TestModule::onDisable() {
