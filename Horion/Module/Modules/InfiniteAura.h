d483b4f3a14141fb1d2b2bdf1bbaf961c407ca8d
AneryCoft <78856933+AneryCoft@users.noreply.github.com>
2023-05-19 20:32:35 +0800

    Update InfiniteAura.h
    Better InfAura #2

--- Horion/Module/Modules/InfiniteAura.h
+++ Horion/Module/Modules/InfiniteAura.h
@@ -1,20 +1,27 @@
 #pragma once
 
-#include "Module.h"
-#include "../ModuleManager.h"
+#include "../Module.h"
+#include "../../ModuleManager.h"
 
 class InfiniteAura : public IModule {
 private:
+	SettingEnum mode;
 	int delay = 15;
-	int Odelay = 0;
-	bool isMulti = false;
-
+	int ticks = 0;
+	float tpDistance = 5.f;
+	std::vector<vec3_t> posList = {};
+	bool renderPos = true;
+	
 public:
-	float range = 15;
+	std::vector<C_Entity*> targetList = {};
+	float range = 100.f;
+
 	InfiniteAura();
 	~InfiniteAura();
 	
 	// Inherited via IModule
 	virtual const char* getModuleName() override;
 	virtual void onTick(C_GameMode* gm) override;
+	virtual void onPreRender(C_MinecraftUIRenderContext* renderCtx) override;
+	virtual void onEnable() override;
 };
