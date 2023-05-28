9716ea253643a08e9f5acc899b107d0bddd0061f
Intoprelised <78622918+Intoprelised@users.noreply.github.com>
2023-05-28 16:40:23 -0400

    BlockSource Virtual Functions instead

--- SDK/BlockLegacy.cpp
+++ SDK/BlockLegacy.cpp
@@ -3,30 +3,18 @@
 #include "../Memory/GameData.h"
 #include "../Utils/Logger.h"
 #include "../Utils/Utils.h"
-Block* BlockSource::getBlock(const Vec3i& block) {
-	using getBlock_t = Block*(__fastcall*)(BlockSource*, const Vec3i&);
-	static getBlock_t getBlock = reinterpret_cast<getBlock_t>(FindSignature("48 89 5C 24 ? 57 48 83 EC ? 48 8B F9 48 8B DA 8B 4A"));
-	return getBlock(this, block);
-}
-BlockActor* BlockSource::getBlockEntity(const Vec3i& block) {
-	using getBlockEntity_t = BlockActor*(__fastcall*)(BlockSource*, const Vec3i&);
-	static getBlockEntity_t getBlockEntity = reinterpret_cast<getBlockEntity_t>(FindSignature("40 55 56 57 48 83 EC ?? 8B 02"));
-	return getBlockEntity(this, block);
-}
-Block* BlockSource::getLiquidBlock(const Vec3i& block) {
-	using getLiquidBlock_t = Block*(__fastcall*)(BlockSource*, const Vec3i&);
-	static getLiquidBlock_t getLiquidBlock = reinterpret_cast<getLiquidBlock_t>(FindSignature("48 89 5C 24 ? 57 48 83 EC 20 48 8B 01 48 8B FA 48 8B D9 FF 50 28"));
-	return getLiquidBlock(this, block);
-}
+
 bool BlockLegacy::getCollisionShape(AABB* collShapeOut, Block* block, BlockSource* blockSource, const Vec3i* pos, Entity* actor) {
 	return Utils::CallVFunc<5, bool, AABB*, Block*, BlockSource*, const Vec3i*, Entity*>(this, collShapeOut, block, blockSource, pos, 0);
 }
+
 void BlockLegacy::liquidGetFlow(Vec3* flowOut, BlockSource* reg, const Vec3i* pos) {
 	using liquid_getFlow_t = int(__fastcall*)(BlockLegacy*, Vec3*, BlockSource*, const Vec3i*);
 	static liquid_getFlow_t liquid_getDepth = Utils::FuncFromSigOffset<liquid_getFlow_t>(FindSignature("E8 ? ? ? ? 48 8B 4C 24 ? F3 0F 10 01"), 1);
 
 	liquid_getDepth(this, flowOut, reg, pos);
 }
+
 bool BlockLegacy::hasWater(BlockSource* reg, const Vec3i& pos) {
 	if (this->material->isLiquid) {
 		return !this->material->isSuperHot;
--- SDK/BlockLegacy.h
+++ SDK/BlockLegacy.h
@@ -80,9 +80,35 @@ public:
 
 class BlockSource {
 public:
-	Block* getBlock(const Vec3i& block);
-
-	BlockActor* getBlockEntity(const Vec3i& block);
-
-	Block* getLiquidBlock(const Vec3i& block);
+	virtual void Destructor();
+	virtual Block* getBlock(int, int, int);
+	virtual Block* getBlock(Vec3i const&);
+	virtual Block* getBlock(Vec3i const&, int);
+	virtual BlockActor* getBlockEntity(Vec3i const&);
+	virtual Block* getExtraBlock(Vec3i const&);
+	virtual Block* getLiquidBlock(Vec3i const&);
+	virtual bool hasBlock(Vec3i const&);
+	virtual bool containsMaterial(AABB const&, struct MaterialType);
+	virtual Material* getMaterial(Vec3i const&);
+	virtual Material* getMaterial(int, int, int);
+	virtual bool hasChunksAt(struct Bounds const&);
+	virtual bool hasChunksAt(Vec3i const&, int);
+	virtual bool hasChunksAt(AABB const&);
+	virtual int getDimensionId(void);
+	virtual __int64 getWeakRef(void);
+	virtual void addListener(struct BlockSourceListener&);
+	virtual void removeListener(BlockSourceListener&);
+	virtual class EntityList* fetchEntities(Entity const*, AABB const&, bool);
+	virtual class EntityList* fetchEntities(__int64, AABB const&, Entity const*);
+	virtual void setBlock(Vec3i const&, Block const&, int, class ActorBlockSyncMessage const*, Entity*);
+	virtual bool containsAnyLiquid(AABB const&);
+	virtual int getMinHeight(void);
+	virtual class Dimension* getDimension(void);
+	virtual class Dimension* getDimensionConst(void);
+	virtual __int64 getILevel(void);
+	virtual std::vector<AABB>* fetchAABBs(AABB const&, bool);
+	virtual std::vector<AABB>* fetchCollisionShapes(AABB const&, float*, bool, class IActorMovementProxy*);
+	virtual AABB* getTallestCollisionShape(AABB const&, float*, bool, IActorMovementProxy*);
+	virtual __int64 getChunkSource(void);
+	virtual bool isSolidBlockingBlock(Vec3i const&);
 };
